3dd4a1348f1cc21982feb598fe30d18ef3fe6220
kfa802 <147632358+kfa802@users.noreply.github.com>
2023-11-23 16:18:55 +0100

    public bounce

--- Assets/Scripts/Spiderweb.cs
+++ Assets/Scripts/Spiderweb.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spiderweb : MonoBehaviour
 {
-    private float bounce = 20f;
+    public float bounce = 20f;
 
 
     private void OnCollisionEnter2D(Collision2D collision)
