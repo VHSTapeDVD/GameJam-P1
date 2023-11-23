e2826e933c8dae6c86b225b1ea36932a07bc1af3
ErikaNicolaisen <147511548+ErikaNicolaisen@users.noreply.github.com>
2023-11-23 19:32:38 +0100

    hej game over igen igen

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -67,6 +68,11 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+
+        if (health == 0)
+        {
+            SceneManager.LoadScene("GamOver");
         }
     }
 }
