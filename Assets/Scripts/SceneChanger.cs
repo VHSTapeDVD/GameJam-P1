df8aac364b69cae06e4ba6712b53d18d4af70387
fanteck-boop <57353353+fanteck-boop@users.noreply.github.com>
2023-11-23 17:29:15 +0100

    lvl 3

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,12 +7,12 @@ public class SceneChanger : MonoBehaviour
 {
     public string sceneName;
 
-    // OnTriggerEnter is called when the Collider other enters the trigger.
-    private void OnTriggerEnter(Collider other)
+    // OnTriggerEnter2D is called when the Collider2D other enters the trigger.
+    private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the colliding object has a specific tag or layer if needed
-        // For example, only trigger when colliding with an object with the "Player" tag
-        if (other.CompareTag("Flag"))
+        // For example, only trigger when colliding with an object with the "Flag" tag
+        if (other.CompareTag("Player"))
         {
             // Load the specified scene
             LoadLevelScene();
