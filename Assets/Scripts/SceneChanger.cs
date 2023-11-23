b9036f5143ad2ed79099d2865d44d9d9f3643baf
VHSTapeDVD <axschoenning@gmail.com>
2023-11-23 13:29:49 +0100

    Modular scenechanger

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public string sceneName;
+
+    private void OntriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag(Flag))
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
+
+
+    }
+ 
+}
